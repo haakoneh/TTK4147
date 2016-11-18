585788be3fa9452f23be6e74930f502f92f33bfc
student <student@Sanntid.(none)>
2016-11-18 14:29:13 +0100

    miniproject nearly finished, need to clean up code

--- Mini_Project/client/part_2/udp_conn.c
+++ Mini_Project/client/part_2/udp_conn.c
@@ -4,9 +4,10 @@
 #include <string.h>
 #include <stdio.h>
 #include <pthread.h>
-#define IP_ADDRESS "192.168.0.1"
-#define STRINGLENGTH 8
-#define AVR_DOUBLE_CHAR_MAX_LENGTH 22
+
+#define IP_ADDRESS 						"192.168.0.1"
+#define STRINGLENGTH 					8
+#define AVR_DOUBLE_CHAR_MAX_LENGTH 	22
 
 pthread_mutex_t udp_send_mutex;
 
@@ -15,11 +16,7 @@ struct udp_conn socket_conn;
 
 
 char *double_to_char(double u_value,char u_arr[]){
-//	#define STRINGLENGTH 8
-	//char u_arr[STRINGLENGTH];
 	sprintf(u_arr, "%f", u_value);
-
-
 }
 
 int udp_init(){
@@ -30,7 +27,6 @@ int udp_init(){
 int send_start(){
 	char msg[] = "START";
 	pthread_mutex_lock(&udp_send_mutex);
-	
 	int ret = udp_send(&socket_conn,msg,strlen(msg)+1);
 	pthread_mutex_unlock(&udp_send_mutex);
 	return ret;
@@ -45,28 +41,20 @@ int send_get(){
 }
 
 int send_set(double u_value){
-	//char u_arr[sizeof(u_value)];
-	//memcpy(&u_arr, &u_value,sizeof(u_value));
-	//char msg[] = "SET:";
-	
 	char number[AVR_DOUBLE_CHAR_MAX_LENGTH]; 
 	double_to_char(u_value,number);
 
 	char msg[AVR_DOUBLE_CHAR_MAX_LENGTH + 5];
 	sprintf(msg,"%s","SET:\0");
 
-
 	strcat(msg,number);
-
-
-
 
 	int ret;
 	pthread_mutex_lock(&udp_send_mutex);
 	ret = udp_send(&socket_conn, msg, strlen(msg)+1);
 	pthread_mutex_unlock(&udp_send_mutex);
-	return ret;
 	
+	return ret;	
 }
 
 int send_signal_ack(){
@@ -84,9 +72,6 @@ int send_stop(){
 	pthread_mutex_unlock(&udp_send_mutex);
 	return ret;
 }
-
-
-
 
 int receive_get(char buf[]){
 	return udp_receive(&socket_conn,buf,256);
