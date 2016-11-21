463e2d80b79ef7ea16c7c68a7c06dfed9056fe58
Name User <straight@hell.gov>
2016-11-21 15:02:50 +0100

    periodic signal thread. Code cleanup

--- Mini_Project/client/part_2/PI_regulator.c
+++ Mini_Project/client/part_2/PI_regulator.c
@@ -9,16 +9,19 @@
 #define KP 				10.0
 #define KI 				800.0
 #define REFERENCE 	1.0
-#define MS 				1000.0
-#define PERIOD_US 	3 * MS
-#define PERIOD_S 		PERIOD_US/(1000.0 * MS)
-#define BUFFER_SIZE 	256
+#define MS 				1000
+#define PERIOD_US 	4 * MS
+#define PERIOD_S 		REG_PERIOD/(1000.0 * MS)
+#define SERVER_DATA_OFFSET 8
+
+#define SIG_PERIOD 257
+#define REG_PERIOD 4019
 
 
 sem_t regulator_sem;
 sem_t signal_sem;
 
-double integral = 0.0;
+float integral = 0.0;
 char regulator_buffer[BUFFER_SIZE];
 
 
@@ -26,25 +29,22 @@ void load_regulator_buffer(char *regulator_value){
 	strcpy(regulator_buffer,regulator_value);
 }
 
-double parse_get(char buffer[]){
-	int i = 0;
-	int j = 0;
-	
-	char our_number[256] ;
-	while(buffer[i++] != ':');
-
-	return atof(buffer + i);
+float parse_get(char buffer[]){
+/* "GET_ACK:123.456" is the only data from the server -
+ 	float starts at index 8 */
+ 	
+	return atof(buffer + SERVER_DATA_OFFSET); 
 }
-double regulator_calculation(double y){
-	double error = REFERENCE - y;
+float regulator_calculation(float y){
+	float error = REFERENCE - y;
 	integral += error * PERIOD_S;
-	double u = KP * error + KI * integral;
+	float u = KP * error + KI * integral;
 
 	return u;
 }
 
 void *receiver(){
-	char recv_buffer[256];
+	char recv_buffer[BUFFER_SIZE];
 
 	while(1){
 		receive_get(recv_buffer);
@@ -60,23 +60,31 @@ void *receiver(){
 }
 
 void *return_sig_ack(){
+	struct timespec time_start;
+	clock_gettime(CLOCK_REALTIME, &time_start);
+	
 	while(1){
 		sem_wait(&signal_sem);
 		send_signal_ack();
+		
+		timespec_add_us(&time_start, SIG_PERIOD);	 
+		clock_nanosleep_the_second(&time_start);
+		clock_gettime(CLOCK_REALTIME,&time_start); 
 	}
 }
 
 void *regulator(){
-	double y = 0.0;
-	double u = 0.0;
+	float y = 0.0;
+	float u = 0.0;
+	
 	struct timespec time_start;
-	//clock_gettime(CLOCK_REALTIME,&time_start); // 2
+	clock_gettime(CLOCK_REALTIME,&time_start); 
 	
-	//send_get();
-	
+	send_start();
+
 	while(1){
-		clock_gettime(CLOCK_REALTIME,&time_start); 
 		send_get();
+		
 		sem_wait(&regulator_sem);
 			
 		y = parse_get(regulator_buffer);
@@ -84,39 +92,35 @@ void *regulator(){
 		
 		send_set(u);
 		
-		timespec_add_us(&time_start, PERIOD_US);	 
-		clock_nanosleep_the_second(&time_start);   
-		//send_get();
-		
+		timespec_add_us(&time_start, REG_PERIOD);	 
+		clock_nanosleep_the_second(&time_start);
+		clock_gettime(CLOCK_REALTIME,&time_start);    
 	}
 }
 
 int main(){
-	pthread_t regulator_thread,signal_thread,receiver_thread;
+	pthread_t 	regulator_thread,
+					signal_thread,
+					receiver_thread;
 
 	memset(regulator_buffer,'\0', sizeof(regulator_buffer));
 	
 	sem_init(&regulator_sem,0,0);
 	sem_init(&signal_sem,0,0);
-	
-	struct timespec time_start;
-	struct timespec time_stop;
 		
 	udp_init();
+	
 	pthread_create(&receiver_thread,NULL,receiver,0);
 	pthread_create(&signal_thread,NULL,return_sig_ack,0);
-	send_start();
 	pthread_create(&regulator_thread,NULL,regulator,0);
 	
-	
-	
-	
-	
+	struct timespec time_stop;
 	clock_gettime(CLOCK_REALTIME,&time_stop);
 	timespec_add_us(&time_stop, 500*MS);
 	clock_nanosleep_the_second(&time_stop);
 
 	send_stop();
+	
 	return 0;
 }
 	
--- Mini_Project/client/part_2/udp_conn.h
+++ Mini_Project/client/part_2/udp_conn.h
@@ -1,15 +1,10 @@
 #include "miniproject.h"
 
-
-
-
-
+#define BUFFER_SIZE 	256
 
 int conn_init();
-
 int send_start();
 int send_get();
-int send_set();
+int send_set(float u_value);
 int sernd_stop();
-
 int receive_get(char *buf);
