231557e9ab16f178b318c35cf2ee1d522d96279b
Name User <straight@hell.gov>
2016-11-22 03:02:59 +0100

    to much too much

--- Mini_Project/client/part_2/PI_regulator.c
+++ Mini_Project/client/part_2/PI_regulator.c
@@ -10,62 +10,74 @@
 #define PERIOD_MS			1000 * BASEPERIOD_US
 #define PERIOD_S 			1000 * PERIOD_MS
 
+#define SIG_PERIOD_US 		257
+
+#define REG_PERIOD_US 		4019
+
 #define KP 					10.0
 #define KI 					800.0
 #define REFERENCE 			1.0
-#define REG_PERIOD_US 		4019
 #define REG_PERIOD_S 		REG_PERIOD_US/(PERIOD_S) 	
 
-#define SIG_PERIOD_US 		257
+
 
 #define SERVER_DATA_OFFSET 	8
 
 sem_t regulator_sem;
 sem_t signal_sem;
+pthread_mutex_t regulator_buffer_mutex;
 
-float integral = 0.0;
-char regulator_buffer[BUFFER_SIZE];
+volatile double integral = 0.0;
+volatile double regulator_buffer = 0;
 
-
-void load_regulator_buffer(char *regulator_value){
-	strcpy(regulator_buffer,regulator_value);
+void load_regulator_buffer(char *new_value, double *regulator_buffer)
+{
+	pthread_mutex_lock(&regulator_buffer_mutex);
+	*regulator_buffer = atof(new_value + SERVER_DATA_OFFSET);
+	pthread_mutex_unlock(&regulator_buffer_mutex);
 }
 
-float parse_get(char buffer[]){
-	return atof(buffer + SERVER_DATA_OFFSET); 
-}
-
-float regulator_calculation(float y){
-	float error = REFERENCE - y;
+double regulator_calculation(double y)
+{
+	double error = REFERENCE - y;
 	integral += error * REG_PERIOD_S;
-	float u = KP * error + KI * integral;
+	double u = KP * error + KI * integral;
 
 	return u;
 }
 
-void *receiver(){
+void *receiver()
+{
 	char recv_buffer[BUFFER_SIZE];
 
-	while(1){
+	while(1)
+	{
 		receive_data(recv_buffer);
-		if(recv_buffer[0] == 'G'){
+
+		if(recv_buffer[0] == 'G')
+		{
 			load_regulator_buffer(recv_buffer);
 			sem_post(&regulator_sem);
-		}else if(recv_buffer[0] == 'S'){
+		}
+		else if(recv_buffer[0] == 'S')
+		{
 			sem_post(&signal_sem);
-		} else {
+		} 
+		else 
+		{
 			// Do nothing
 		}
 	}
 }
 
-void *return_sig_ack(){
-
+void *return_sig_ack()
+{
 	struct timespec time_start;
 	clock_gettime(CLOCK_REALTIME, &time_start);
 	timespec_add_us(&time_start, SIG_PERIOD);
 	
-	while(1){
+	while(1)
+	{
 		sem_wait(&signal_sem);
 		send_signal_ack();
 			 
@@ -76,9 +88,10 @@ void *return_sig_ack(){
 	}
 }
 
-void *regulator(){
-	float y = 0.0;
-	float u = 0.0;
+void *regulator()
+{
+	double y = 0.0;
+	double u = 0.0;
 	
 	struct timespec time_start;
 	clock_gettime(CLOCK_REALTIME,&time_start);
@@ -86,12 +99,16 @@ void *regulator(){
 	
 	send_start();
 
-	while(1){
+	while(1)
+	{
 		send_get();
 		
-		sem_wait(&regulator_sem);
-			
-		y = parse_get(regulator_buffer);
+		sem_wait(&regulator_sem);	
+		
+		pthread_mutex_lock(&regulator_buffer_mutex);
+		y = regulator_buffer;
+		pthread_mutex_unlock(&regulator_buffer_mutex);
+
 		u = regulator_calculation(y);
 		
 		send_set(u);
@@ -103,15 +120,16 @@ void *regulator(){
 	}
 }
 
-int main(){
+int main()
+{
 	pthread_t 	regulator_thread,
 				signal_thread,
 				receiver_thread;
-
-	memset(regulator_buffer,'\0', sizeof(regulator_buffer));
 	
 	sem_init(&regulator_sem,0,0);
 	sem_init(&signal_sem,0,0);
+
+	pthread_mutex_init(&regulator_buffer_mutex, NULL);
 		
 	udp_init();
 	
--- Mini_Project/client/part_2/udp_conn.c
+++ Mini_Project/client/part_2/udp_conn.c
@@ -6,13 +6,15 @@
 #include <pthread.h>
 
 #define IP_ADDRESS 						"192.168.0.1"
-#define AVR_FLOAT_CHAR_MAX_LENGTH 	22
+#define DOUBLE_TO_CHAR_LENGTH 	23  
 
 struct udp_conn socket_conn;
 pthread_mutex_t udp_send_mutex;
 
-int send_with_mutex(char *data){
+int send_with_mutex(char *data)
+{
 	int ret;
+	
 	pthread_mutex_lock(&udp_send_mutex);
 	ret = udp_send(&socket_conn, data, strlen(data)+1);
 	pthread_mutex_unlock(&udp_send_mutex);
@@ -20,52 +22,47 @@ int send_with_mutex(char *data){
 	return ret;
 }
 
-int udp_init(){
+int udp_init()
+{	
+	pthread_mutex_init(&udp_send_mutex, NULL);
 	
-	pthread_mutex_init(&udp_send_mutex,NULL);
-	
-	int ret;
-	ret = udp_init_client(&socket_conn,9999,IP_ADDRESS);
-	
-	int i = 0;
-	
-	/* Get rid of lag on first send */
-	for (i = 0; i<10; i++){
-		udp_send(&socket_conn, "WWW", 4);
-	}
-	
-	return ret;
+	return udp_init_client(&socket_conn, 9999, IP_ADDRESS);
 }
 
-void *float_to_char(float u_value,char *u_arr){
-	sprintf(u_arr, "%f", u_value);
-}
-
-int send_start(){
+int send_start()
+{
 	char msg[] = "START";
+	
 	return send_with_mutex(msg);
 }
 
-int send_get(){
+int send_get()
+{
 	char msg[] = "GET";
+	
 	return send_with_mutex(msg);
 }
 
-int send_signal_ack(){
+int send_signal_ack()
+{
 	char msg[] = "SIGNAL_ACK";
+	
 	return send_with_mutex(msg);	
 }
 
-int send_stop(){
+int send_stop()
+{
 	char msg[] = "STOP";
+	
 	return send_with_mutex(msg);	
 }
 
-int send_set(float u_value){
-	char number[AVR_FLOAT_CHAR_MAX_LENGTH]; 
+int send_set(double u_value)
+{
+	char number[DOUBLE_TO_CHAR_LENGTH]; 
 	sprintf(number, "%f", u_value);
 
-	char msg[AVR_FLOAT_CHAR_MAX_LENGTH + 5];
+	char msg[DOUBLE_TO_CHAR_LENGTH + 5];
 	sprintf(msg,"%s","SET:\0");
 
 	strcat(msg,number);
@@ -73,8 +70,9 @@ int send_set(float u_value){
 	return send_with_mutex(msg);	
 }
 
-int receive_data(char buf[]){
-	return udp_receive(&socket_conn,buf,BUFFER_SIZE );
+int receive_data(char *buf)
+{
+	return udp_receive(&socket_conn, buf, BUFFER_SIZE);
 }
 
 
